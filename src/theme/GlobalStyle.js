f0414d61d882ec67febf34468ee037d3fb665303
AWydra <hello@awydra.pl>
2020-01-27 14:08:38 +0100

    Created global styles

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: "Roboto", sans-serif;
+  }
+`;
+
+export default GlobalStyle;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Button = styled.button`
   background-color: magenta;
   margin: 0 0 0 9px;
 `;
 
-function App() {
+function Root() {
   return (
     <>
+      <GlobalStyle />
       <h1>Hello World!</h1>
       <Button>Click me!</Button>
     </>
   );
 }
 
-export default App;
+export default Root;
