b83f5d0c7fbcc5b721135c777e96fabe7c3d35f0
AWydra <hello@awydra.pl>
2020-01-27 12:53:26 +0100

    configured lint-staged with husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  //"plugin:react/recommended",
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
