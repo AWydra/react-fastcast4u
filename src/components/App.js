d06cabe9c7411319d1592d5ae772ffe589279757
AWydra <hello@awydra.pl>
2020-01-27 13:00:52 +0100

    Added styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: magenta;
+  margin: 0 0 0 9px;
+`;
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button>Click me!</Button>
+    </>
+  );
 }
 
 export default App;
