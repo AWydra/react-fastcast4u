0159270d4c1583197227a13363cbb6610f93a03c
AWydra <hello@awydra.pl>
2020-01-30 18:07:25 +0100

    Imroved components with MaterialUI system

--- src/components/atoms/Image/Image.js
+++ src/components/atoms/Image/Image.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { spacing } from '@material-ui/system';
 
 const Image = styled.img`
+  ${spacing}
+  max-width: 100%;
   display: block;
   border-style: none;
 `;
