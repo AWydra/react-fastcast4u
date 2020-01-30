cb773c8b88b0547c394de04f0f5a8bed26a31731
AWydra <hello@awydra.pl>
2020-01-30 12:13:55 +0100

    Created Hero Section

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,48 @@
+// @ts-nocheck
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Typography as MuiTypography, Button } from '@material-ui/core';
+import RocketIcon from 'assets/svg/RocketIcon';
+import FullContainer from 'components/atoms/FullContainer';
+import theme from 'theme/mainTheme';
+
+const Typography = styled(MuiTypography)`
+  font-weight: 700;
+
+  ${theme.breakpoints.up('lg')} {
+  }
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: ${theme.spacing(4)}px;
+  padding: 0 ${theme.spacing(4)}px;
+
+  .MuiButton-label {
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 3;
+  }
+`;
 
 const Home = () => {
-  return <Link to="/asdasd">Page 404</Link>;
+  return (
+    <FullContainer center maxWidth="xl">
+      <Typography component="h1" variant="h2" align="center" gutterBottom>
+        Create Your Own Internet Radio Station
+      </Typography>
+      <StyledButton
+        component={Link}
+        to="/asdasd"
+        variant="contained"
+        color="primary"
+        size="large"
+        endIcon={<RocketIcon />}
+      >
+        Start Now
+      </StyledButton>
+    </FullContainer>
+  );
 };
 
 export default Home;
