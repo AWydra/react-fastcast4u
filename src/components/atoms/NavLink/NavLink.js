34717e18bfb91e42372e9f2b2f1987d868f36637
AWydra <hello@awydra.pl>
2020-02-04 17:52:12 +0100

    Integrated NavLink with react-router

--- src/components/atoms/NavLink/NavLink.js
+++ src/components/atoms/NavLink/NavLink.js
@@ -1,15 +1,30 @@
+// @ts-nocheck
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ButtonBase } from '@material-ui/core';
+import { spacing } from '@material-ui/system';
 import theme from 'theme/footerTheme';
 
-const NavLink = styled(ButtonBase)`
-  padding: ${theme.spacing(0.75)}px ${theme.spacing(1)}px;
+const NavButton = styled(ButtonBase)`
+  ${spacing}
   transition: ${theme.transitions.create('all')};
   font-size: ${theme.typography.pxToRem(17)};
 
   &:hover {
-    color: ${theme.palette.primary.light};
+    color: ${theme.palette.grey[500]};
   }
 `;
+
+const NavLink = ({ children, ...props }) => (
+  <NavButton component={Link} focusRipple {...props}>
+    {children}
+  </NavButton>
+);
+
+NavLink.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
+};
 
 export default NavLink;
