54e08b30f856a99c131f67093c0639d9e6167bc1
AWydra <hello@awydra.pl>
2020-02-04 11:44:20 +0100

    Created theme for footer

---
+++ src/theme/footerTheme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+const footerTheme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1280,
+    },
+  },
+  palette: {
+    type: 'dark',
+    primary: {
+      main: 'hsl(204, 63%, 49%)',
+    },
+    secondary: {
+      main: 'hsl(350, 65%, 46%);',
+    },
+  },
+});
+
+export default responsiveFontSizes(footerTheme);
