09bbbf6d1d9efa3fb17ac726fc7bb4a67887f258
AWydra <hello@awydra.pl>
2020-02-20 21:55:51 +0100

    Refactored package grid

--- src/templates/PackageGrid.js
+++ src/templates/PackageGrid.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    [theme.breakpoints.up('md')]: {
-      padding: theme.spacing(3),
-    },
-  },
-}));
-
-const PackageGrid = ({ children }) => {
-  const classes = useStyles();
-
-  return (
-    <Grid container spacing={2} className={classes.root}>
-      {children.map((el, i) => (
-        <Grid key={i} item xs={12} lg={12} xl={6}>
-          {el}
-        </Grid>
-      ))}
-    </Grid>
-  );
-};
+const PackageGrid = ({ children }) => (
+  <Grid container spacing={2}>
+    {children.map((el, i) => (
+      <Grid key={i} item xs={12} lg={6}>
+        {el}
+      </Grid>
+    ))}
+  </Grid>
+);
 
 PackageGrid.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
