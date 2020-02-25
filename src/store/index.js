740eb8c5dd34f797910e80844faa56c079427714
AWydra <hello@awydra.pl>
2020-02-25 15:51:18 +0100

    Integrated redux with devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createStore, combineReducers } from 'redux';
 import orderReducer from 'reducers/orderReducer';
 import orderActions from 'actions/orderActions';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   order: orderReducer,
 });
 
-const store = createStore(rootReducer);
-window.store = store;
+const store = createStore(rootReducer, composeWithDevTools());
 
 store.dispatch(orderActions.add('AdMob'));
 
