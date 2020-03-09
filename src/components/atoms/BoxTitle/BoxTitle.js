cbb1c657666070be89ad698a617eb4339b5c873e
Arkadiusz Wydra <hello@awydra.pl>
2020-03-09 18:46:10 +0100

    Created styled title box

---
+++ src/components/atoms/BoxTitle/BoxTitle.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Text from 'components/atoms/Text/Text';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    // fontSize: '2rem',
+    textAlign: 'center',
+    fontWeight: 700,
+  },
+  color: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+const BoxTitle = ({ children, ...props }) => {
+  const classes = useStyles();
+
+  const sentence = children.split(' ').map(word => `${word} `);
+  if (sentence.length >= 2) {
+    sentence[1] = <span className={classes.color}>{sentence[1]} </span>;
+  }
+
+  return (
+    <Text className={classes.title} {...props}>
+      {sentence.map((word, i) => (
+        <React.Fragment key={i}>{word}</React.Fragment>
+      ))}
+    </Text>
+  );
+};
+
+BoxTitle.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+export default BoxTitle;
