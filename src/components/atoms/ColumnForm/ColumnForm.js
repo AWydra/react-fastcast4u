04c645ec698424624a942c196f3dbce9416426ba
Arkadiusz Wydra <hello@awydra.pl>
2020-03-11 01:17:08 +0100

    Fixed column form apperance

--- src/components/atoms/ColumnForm/ColumnForm.js
+++ src/components/atoms/ColumnForm/ColumnForm.js
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 import theme from 'theme/mainTheme';
 import themeUtils from 'utils/theme';
 
-const ColumnForm = styled.form`
+const ColumnForm = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 450px;
   padding: ${theme.spacing(3, 2)};
   border: solid ${themeUtils.modeSwitch(theme.palette.grey[400], theme.palette.grey[700])} 1px;
+  border-radius: 4px;
   background-color: ${theme.palette.background.paper};
+
+  ${theme.breakpoints.up('md')} {
+    padding: ${theme.spacing(3)}px;
+  }
 `;
 
 export default ColumnForm;
