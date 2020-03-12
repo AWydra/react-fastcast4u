0a3ff970a18f34bc2447db8518ca5a1d366247a6
Arkadiusz Wydra <hello@awydra.pl>
2020-03-12 15:06:10 +0100

    Created order payment page

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import OrderHome from 'views/Order/OrderHome/OrderHome';
 import OrderPackage from 'views/Order/OrderPackage/OrderPackage';
 import OrderLogin from 'views/Order/OrderLogin/OrderLogin';
+import OrderPayment from 'views/Order/OrderPayment/OrderPayment';
 import NotFound from 'views/NotFound/NotFound';
 
 const Order = () => (
@@ -11,6 +12,7 @@ const Order = () => (
     <Route exact path="/order" component={OrderHome} />
     <Route path="/order/package" component={OrderPackage} />
     <Route path="/order/login" component={OrderLogin} />
+    <Route path="/order/payment" component={OrderPayment} />
     <Route component={NotFound} />
   </Switch>
 );
---
+++ src/views/Order/OrderPayment/OrderPayment.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button, makeStyles, useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import FullContainer from 'components/atoms/FullContainer/FullContainer';
+import ColumnForm from 'components/atoms/ColumnForm/ColumnForm';
+import BoxTitle from 'components/atoms/BoxTitle/BoxTitle';
+import Stepper from 'components/organisms/Stepper/Stepper';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    width: 200,
+    margin: theme.spacing(1.5, 0),
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
+    fontWeight: 600,
+  },
+}));
+
+const ButtonContainer = styled.div`
+  padding: 8px 0 32px;
+`;
+
+const OrderPayment = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+  return (
+    <FullContainer center>
+      {matches && (
+        <Stepper
+          steps={['Create your Server Package', 'Create Account', 'Payment & Setup']}
+          activeStep={2}
+          mb={6}
+        />
+      )}
+      <ColumnForm>
+        <BoxTitle variant="h5" component="h1" mb={2}>
+          Payment Checkout
+        </BoxTitle>
+        <ButtonContainer>
+          <Button
+            className={`${classes.button}`}
+            variant="contained"
+            size="large"
+            color="secondary"
+          >
+            Debit / Credit Card
+          </Button>
+          <Button className={`${classes.button}`} variant="contained" size="large" color="primary">
+            PayPal
+          </Button>
+        </ButtonContainer>
+        <Button component={Link} to="/order/login" variant="contained" color="primary">
+          Back
+        </Button>
+      </ColumnForm>
+    </FullContainer>
+  );
+};
+
+export default OrderPayment;
