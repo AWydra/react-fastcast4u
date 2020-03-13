5773777ee5c9249890b540368fdf07fa68b89660
AWydra <hello@awydra.pl>
2020-03-13 22:33:34 +0100

    Resolved problem with disappearing styled component styles in build version

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,700&display=swap');
-
   *, *::before, *::after {
     box-sizing: border-box;
   }
