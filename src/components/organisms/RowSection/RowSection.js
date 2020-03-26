3abde95e29383a9ba67332853f98856ae6d14bdc
AWydra <hello@awydra.pl>
2020-03-26 19:43:34 +0100

    Refactored row section component

--- src/components/organisms/RowSection/RowSection.js
+++ src/components/organisms/RowSection/RowSection.js
@@ -1,68 +1,57 @@
 // @ts-nocheck
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
-import { Grid, Box } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import RowContent from 'components/molecules/RowContent/RowContent';
 import Image from 'components/atoms/Image/Image';
 
-const StyledContainer = styled(Grid)`
-  ${({ theme }) =>
-    css`
-      padding: ${theme.spacing(4)}px 0;
-    `}
-`;
+const useStyles = makeStyles(theme => ({
+  container: {
+    padding: theme.spacing(4, 0),
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: reverse => (reverse ? 'flex-end' : 'flex-start'),
+    align: reverse => (reverse ? 'right' : 'left'),
+  },
+  image: {
+    padding: theme.spacing(0, 2),
 
-const StyledGrid = styled(({ reverse, children, ...props }) => (
-  <Box component={Grid} {...props}>
-    {children}
-  </Box>
-))`
-  ${({ theme }) => css`
-    img {
-      margin: 0 auto;
-      padding: 0 ${theme.spacing(8)}px;
-    }
+    '& img': {
+      margin: '0 auto',
+      padding: theme.spacing(0, 8),
+    },
 
-    ${({ reverse }) =>
-      reverse &&
-      `
-    order: 1;
-  `}
+    [theme.breakpoints.up('lg')]: {
+      order: reverse => (reverse ? 1 : 0),
+    },
 
-    ${theme.breakpoints.down('md')} {
-      padding: 0 ${theme.spacing(8)}px;
-      order: unset;
-      img {
-        padding: 0;
-      }
-    }
+    [theme.breakpoints.down('md')]: {
+      order: 'unset',
+      padding: theme.spacing(0, 4),
+      '& img': {
+        padding: theme.spacing(0),
+      },
+    },
+  },
+}));
 
-    ${theme.breakpoints.down('xs')} {
-      padding: 0 ${theme.spacing(4)}px;
-    }
-  `}
-`;
+const RowSection = ({ img, reverse, ...props }) => {
+  const classes = useStyles(reverse);
 
-const RowSection = ({ img, reverse, ...props }) => (
-  <StyledContainer container>
-    <StyledGrid item xs={12} lg={7} reverse={reverse}>
-      <Image src={img} />
-    </StyledGrid>
-    <StyledGrid
-      item
-      xs={12}
-      lg={5}
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems={reverse ? 'flex-end' : 'flex-start'}
-      align={reverse ? 'right' : 'left'}
-    >
-      <RowContent {...props} reverse={reverse} />
-    </StyledGrid>
-  </StyledContainer>
-);
+  return (
+    <Grid className={classes.container} container>
+      <Grid className={classes.image} item xs={12} lg={7}>
+        <Image src={img} />
+      </Grid>
+      <Grid className={classes.item} item xs={12} lg={5}>
+        <RowContent reverse={reverse} {...props} />
+      </Grid>
+    </Grid>
+  );
+};
 
 RowSection.propTypes = {
   img: PropTypes.string.isRequired,
