74f94e010a744faac4795d8f1361b00177e88024
AWydra <hello@awydra.pl>
2020-03-26 19:42:28 +0100

    Adapted alert to bottom menu

--- src/components/atoms/Alert/Alert.js
+++ src/components/atoms/Alert/Alert.js
@@ -1,14 +1,23 @@
 // @ts-nocheck
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Snackbar, useMediaQuery, useTheme } from '@material-ui/core';
+import { Snackbar, useMediaQuery, useTheme, makeStyles } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles(theme => ({
+  snackbar: {
+    marginBottom: matches => matches && theme.spacing(8),
+    bottom: theme.spacing(1),
+  },
+}));
 
 const Alert = ({ severity, children, ...props }) => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const classes = useStyles(matches);
   return (
     <Snackbar
+      className={classes.snackbar}
       autoHideDuration={6000}
       anchorOrigin={{ vertical: 'bottom', horizontal: matches ? 'center' : 'left' }}
       {...props}
