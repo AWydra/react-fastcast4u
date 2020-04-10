c96ba605646add882bcfd982727105173d5d667b
AWydra <hello@awydra.pl>
2020-04-10 23:00:09 +0200

    Created advertisement component

---
+++ src/components/molecules/Ad/Ad.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  makeStyles,
+} from '@material-ui/core';
+import Text from 'components/atoms/Text/Text';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(3),
+    position: 'sticky',
+    top: theme.spacing(3),
+  },
+  media: {
+    padding: theme.spacing(2.5),
+  },
+  content: {
+    padding: theme.spacing(3, 0),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'white',
+    [theme.breakpoints.up('lg')]: {
+      padding: theme.spacing(3),
+    },
+  },
+  text: {
+    textAlign: 'center',
+  },
+}));
+
+const Ad = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea component={Link} to="/order">
+        <CardMedia
+          className={classes.media}
+          image="//fastcast4u.com/radio-directory/images/directory-banner.png"
+          title="Contemplative Reptile"
+        >
+          <CardContent className={classes.content}>
+            <Text className={classes.text} mb={4} variant="h4" component="h2">
+              Start Your Online Radio Station for&nbsp;$1
+            </Text>
+            <Button size="large" variant="contained" color="primary">
+              Start Now
+            </Button>
+          </CardContent>
+        </CardMedia>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default Ad;
