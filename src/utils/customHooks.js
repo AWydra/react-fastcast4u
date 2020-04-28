3fa938399ff4e0a94ab96e8a11c70d7b44ced646
AWydra <hello@awydra.pl>
2020-04-28 19:33:05 +0200

    Refactored useDidUpdate hook

--- src/utils/customHooks.js
+++ src/utils/customHooks.js
@@ -5,9 +5,11 @@ import generalActions from 'actions/generalActions';
 export const useAlert = () => {
   const [alert, setAlert] = useState({});
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(generalActions.setAlert(alert));
   }, [alert, dispatch]);
+
   return {
     success: content => setAlert({ content, type: 'success', open: true }),
     warning: content => setAlert({ content, type: 'warning', open: true }),
@@ -18,17 +20,17 @@ export const useAlert = () => {
   };
 };
 
-export const useDidUpdate = (callback, deps) => {
-  const hasMount = useRef(false);
-
+export const useDidUpdate = (fn, conditions) => {
+  const didMountRef = useRef(false);
   useEffect(() => {
-    if (hasMount.current) {
-      callback();
-    } else {
-      hasMount.current = true;
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
     }
+
+    return fn && fn();
     // eslint-disable-next-line
-  }, deps);
+  }, conditions);
 };
 
 export default { useAlert, useDidUpdate };
