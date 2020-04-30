c17023f5d0c7954b2b3ad8d6f5cece36dc760d7a
AWydra <hello@awydra.pl>
2020-04-30 17:02:04 +0200

    Created SVG play icon

---
+++ src/assets/svg/PlayIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const PlayIcon = props => (
+  <SvgIcon {...props}>
+    <path d="M6 2L6 22.36L22 12.18L6 2Z" />
+  </SvgIcon>
+);
+
+export default PlayIcon;
