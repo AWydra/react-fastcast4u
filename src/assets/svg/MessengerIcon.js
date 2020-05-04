4eb7797222f5b9cc839c953b59e5f5d1369f9c71
AWydra <hello@awydra.pl>
2020-05-04 19:59:07 +0200

    Added messenger svg icon

---
+++ src/assets/svg/MessengerIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const MessengerIcon = props => (
+  <SvgIcon {...props}>
+    <path d="M22 11.26C22 16.38 17.52 20.52 12 20.52C11 20.52 10.04 20.38 9.13 20.13L5.73 22L5.73 18.47C3.46 16.77 2 14.17 2 11.26C2 6.14 6.48 2 12 2C17.52 2 22 6.15 22 11.26ZM18.46 8.67L13.55 11.38L10.95 8.67L5.48 14.47L10.45 11.75L12.99 14.47L18.46 8.67Z" />
+  </SvgIcon>
+);
+
+export default MessengerIcon;
