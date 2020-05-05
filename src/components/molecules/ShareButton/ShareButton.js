bb2e0ae1583b92e7f5f621e765e5369ab7e353f7
AWydra <hello@awydra.pl>
2020-05-05 18:20:28 +0200

    Created share button

---
+++ src/components/molecules/ShareButton/ShareButton.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import copy from 'copy-to-clipboard';
+import { Menu, MenuItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import IconButtonWithLabel from 'components/atoms/IconButtonWithLabel/IconButtonWithLabel';
+import ShareIcon from '@material-ui/icons/ShareOutlined';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import MessengerIcon from 'assets/svg/MessengerIcon';
+import shareLinkVia from 'utils/shareLinkVia';
+
+const ShareButton = ({ id, ...props }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleCopy = () => {
+    copy(shareLinkVia.copy(id));
+    setAnchorEl(null);
+  };
+
+  const handleFacebook = () => {
+    shareLinkVia.facebook(id);
+    setAnchorEl(null);
+  };
+
+  const handleTwitter = () => {
+    shareLinkVia.twitter('123');
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButtonWithLabel
+        onClick={handleClick}
+        icon={ShareIcon}
+        label="Share"
+        {...props}
+        aria-controls="share-menu"
+        aria-haspopup="true"
+      />
+      <Menu
+        id="share-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleCopy}>
+          <ListItemIcon>
+            <FileCopyOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText primary="Copy" />
+        </MenuItem>
+        <MenuItem onClick={handleFacebook}>
+          <ListItemIcon>
+            <FacebookIcon />
+          </ListItemIcon>
+          <ListItemText primary="Facebook" />
+        </MenuItem>
+        <MenuItem
+          component="a"
+          href={shareLinkVia.messenger('123')}
+          target="_blank"
+          onClick={() => setAnchorEl(null)}
+        >
+          <ListItemIcon>
+            <MessengerIcon />
+          </ListItemIcon>
+          <ListItemText primary="Messenger" />
+        </MenuItem>
+        <MenuItem onClick={handleTwitter}>
+          <ListItemIcon>
+            <TwitterIcon />
+          </ListItemIcon>
+          <ListItemText primary="Twitter" />
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+ShareButton.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
+
+export default ShareButton;
