f3a7bc57ba6d75c01e23e2fff8d4f620b7c9dde0
AWydra <hello@awydra.pl>
2020-05-07 12:13:57 +0200

    Fixed alert displaying on directory page with player

--- src/components/atoms/Alert/Alert.js
+++ src/components/atoms/Alert/Alert.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Snackbar, useMediaQuery, useTheme, makeStyles } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
@@ -7,17 +8,20 @@ import { useAlert } from 'utils/customHooks';
 
 const useStyles = makeStyles(theme => ({
   snackbar: {
-    marginBottom: matches => matches && theme.spacing(8),
-    bottom: matches => matches && theme.spacing(1),
+    marginBottom: props =>
+      props.showPlayer && props.location.pathname.includes('radio-directory') && theme.spacing(6),
+    bottom: props => props.matches && theme.spacing(9),
   },
 }));
 
 const Alert = () => {
   const alert = useAlert();
   const reduxAlert = useSelector(state => state.general.alert);
+  const showPlayer = useSelector(state => state.directory.player.show);
   const theme = useTheme();
+  const location = useLocation();
   const matches = useMediaQuery(theme.breakpoints.down('sm'));
-  const classes = useStyles(matches);
+  const classes = useStyles({ matches, showPlayer, location });
 
   return (
     <Snackbar
