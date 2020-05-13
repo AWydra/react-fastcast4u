fe1bbaccabdca893efbac92112117c8f206841a2
AWydra <hello@awydra.pl>
2020-05-13 20:01:18 +0200

    Created status component

---
+++ src/components/atoms/Status/Status.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
+import Text from 'components/atoms/Text/Text';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+const useStyles = makeStyles(theme => ({
+  text: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: theme.typography.pxToRem(18),
+    fontWeight: 600,
+  },
+  icon: {
+    fontSize: theme.typography.pxToRem(26),
+    color: active => (active ? theme.palette.success.main : theme.palette.error.main),
+  },
+}));
+
+const Status = ({ active }) => {
+  const classes = useStyles(active);
+
+  return (
+    <Text className={classes.text}>
+      <FiberManualRecordIcon className={classes.icon} />
+      {active ? 'Available' : 'Not Available'}
+    </Text>
+  );
+};
+
+Status.defaultProps = {
+  active: true,
+};
+
+Status.propTypes = {
+  active: PropTypes.bool,
+};
+
+export default Status;
