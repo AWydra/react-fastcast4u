4781fed1731e3adf68aedf1bcac16cad924cf4ec
AWydra <hello@awydra.pl>
2020-05-19 17:57:19 +0200

    Refactored price calculation in premium box component

--- src/components/molecules/PackageBox/PremiumBox.js
+++ src/components/molecules/PackageBox/PremiumBox.js
@@ -1,23 +1,31 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import orderActions from 'actions/orderActions';
 import Base from './Base';
 
-const data = {
-  id: 'Premium stream quality - 320kbps',
-  name: 'Premium stream quality - 320kbps',
-  description: 'The highest online audio streaming bitrate for the best sound quality',
-  monthly: '10.00',
-  annually: '120.00',
-  biennially: '240.00',
-};
+const countPrice = (product, cycle) => product[cycle].premium - product[cycle].regular;
 
 const PremiumBox = () => {
   const dispatch = useDispatch();
   const cycle = useSelector(state => state.order.cycle);
   const plan = useSelector(state => state.order.plan);
+  const activeProduct = useSelector(state => state.order.activeProduct);
+  const products = useSelector(state => state.order.products);
+  const product = products.find(el => el.id === activeProduct);
   const isActive = plan === 'premium';
+
+  const data = useMemo(
+    () => ({
+      id: 'Premium stream quality - 320kbps',
+      name: 'Premium stream quality - 320kbps',
+      description: 'The highest online audio streaming bitrate for the best sound quality',
+      monthly: countPrice(product, 'monthly'),
+      annually: countPrice(product, 'annually'),
+      biennially: countPrice(product, 'biennially'),
+    }),
+    [product],
+  );
 
   const handleClick = () => {
     dispatch(orderActions.setPlan(plan === 'regular' ? 'premium' : 'regular'));
