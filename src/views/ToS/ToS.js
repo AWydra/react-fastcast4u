fa7406c2f2100fa1e95632fda37eacb7ae6036bc
AWydra <hello@awydra.pl>
2020-05-25 17:00:02 +0200

    Created tos page

---
+++ src/views/ToS/ToS.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Box, LinearProgress, makeStyles } from '@material-ui/core';
+import generalServices from 'services/general';
+import FullContainer from 'components/atoms/FullContainer/FullContainer';
+
+const useStyles = makeStyles(theme => ({
+  tos: {
+    '& h1': theme.typography.h4,
+    '& p': theme.typography.h6,
+    '& li': theme.typography.body1,
+  },
+  loading: {
+    marginTop: theme.spacing(8),
+  },
+}));
+
+const Privacy = () => {
+  const [response, setResponse] = useState('');
+  const classes = useStyles();
+
+  useEffect(() => {
+    const getPrivacyPolicy = async () => {
+      const response = await generalServices.getTos();
+      setResponse(response);
+    };
+
+    getPrivacyPolicy();
+  }, []);
+
+  return (
+    <FullContainer maxWidth="xl">
+      {response ? (
+        <Box mt={4} className={classes.tos} dangerouslySetInnerHTML={{ __html: response }} />
+      ) : (
+        <LinearProgress className={classes.loading} />
+      )}
+    </FullContainer>
+  );
+};
+
+export default Privacy;
