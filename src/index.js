1b3c9888c091b0e341b7581ef462b37b157036ac
AWydra <hello@awydra.pl>
2020-06-24 13:08:31 +0200

    Implemented PWA

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onUpdate: async registration => {
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      window.location.reload();
+    }
+  },
+});
