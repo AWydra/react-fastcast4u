db1d6b4073a28d559a50a8cb0ba7ac9925c123e9
AWydra <hello@awydra.pl>
2020-06-30 17:56:05 +0200

    Optimized slides in row section component

--- src/components/organisms/RowSection/RowSection.js
+++ src/components/organisms/RowSection/RowSection.js
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, makeStyles, Slide } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import RowContent from 'components/molecules/RowContent/RowContent';
 import Image from 'components/atoms/Image/Image';
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import { Slide } from 'react-awesome-reveal';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -60,29 +60,18 @@ const useStyles = makeStyles(theme => ({
 
 const RowSection = ({ img, reverse, heading, long, ...props }) => {
   const classes = useStyles({ reverse, long });
-  const [show, setShow] = useState(false);
 
   return (
-    <LazyLoadComponent
-      threshold={0}
-      height={380}
-      afterLoad={() => {
-        setShow(true);
-      }}
-    >
-      <Grid className={classes.container} container>
-        <Slide direction={reverse ? 'left' : 'right'} in={show} timeout={{ enter: 900 }}>
-          <Grid className={classes.image} item xs={12} md={long ? 6 : 7}>
-            {React.isValidElement(img) ? img : <Image src={img} alt={heading} />}
-          </Grid>
-        </Slide>
-        <Slide direction={reverse ? 'right' : 'left'} in={show} timeout={{ enter: 900 }}>
-          <Grid className={classes.item} item xs={12} md={long ? 6 : 5}>
-            <RowContent long={long} heading={heading} {...props} />
-          </Grid>
+    <Grid className={classes.container} container>
+      <Grid className={classes.image} item xs={12} md={long ? 6 : 7}>
+        <Slide direction={reverse ? 'right' : 'left'} triggerOnce>
+          {React.isValidElement(img) ? img : <Image src={img} alt={heading} />}
         </Slide>
       </Grid>
-    </LazyLoadComponent>
+      <Grid className={classes.item} item xs={12} md={long ? 6 : 5}>
+        <RowContent long={long} heading={heading} {...props} />
+      </Grid>
+    </Grid>
   );
 };
 
