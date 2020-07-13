90bfa99de30dd4d1c3dd28a13c93f733a25a3784
AWydra <hello@awydra.pl>
2020-07-13 17:56:59 +0200

    Created promobar component

---
+++ src/components/organisms/Promobar/Promobar.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Box, Button, Container, makeStyles } from '@material-ui/core';
+import Text from 'components/atoms/Text/Text';
+
+const useStyles = makeStyles(theme => ({
+  box: {
+    margin: theme.spacing(2, 0, 4),
+    padding: theme.spacing(4, 0),
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexWrap: 'wrap',
+    },
+  },
+  content: {
+    paddingRight: theme.spacing(2),
+    flexGrow: 1,
+    [theme.breakpoints.down('xs')]: {
+      textAlign: 'center',
+    },
+  },
+  buttonContainer: {
+    flexShrink: 0,
+    [theme.breakpoints.down('xs')]: {
+      marginTop: theme.spacing(2),
+    },
+  },
+  button: {
+    color: 'white',
+    borderColor: 'white',
+  },
+}));
+
+const Promobar = ({ primary, secondary, button }) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.box}>
+      <Container className={classes.container} maxWidth="xl">
+        <Box className={classes.content}>
+          <Text component="h2" variant="h5" fontWeight={500}>
+            {primary}
+          </Text>
+          {secondary && <Text mt={2}>{secondary}</Text>}
+        </Box>
+        <Box className={classes.buttonContainer}>
+          <Button
+            className={classes.button}
+            component={Link}
+            variant="outlined"
+            size="large"
+            {...button}
+          >
+            {button.label}
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+Promobar.defaultProps = {
+  secondary: '',
+};
+
+Promobar.propTypes = {
+  primary: PropTypes.string.isRequired,
+  secondary: PropTypes.string,
+  button: PropTypes.shape({
+    label: PropTypes.string,
+  }).isRequired,
+};
+
+export default Promobar;
