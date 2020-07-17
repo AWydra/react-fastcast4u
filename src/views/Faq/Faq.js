0846a4d964656aec80f96081d02c93cf6fdd1809
AWydra <hello@awydra.pl>
2020-07-17 18:28:29 +0200

    Improved responsiveness on /faq

--- src/views/Faq/Faq.js
+++ src/views/Faq/Faq.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
-import { Box, Paper, Tabs, Tab, makeStyles } from '@material-ui/core';
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  Paper,
+  Tabs,
+  Tab,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+} from '@material-ui/core';
 import FullContainer from 'components/atoms/FullContainer/FullContainer';
 import HeadingBlock from 'components/molecules/HeadingBlock/HeadingBlock';
 import Accordion from 'components/organisms/Accordion/Accordion';
@@ -51,6 +62,9 @@ const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     alignItems: 'flex-start',
+    [theme.breakpoints.down('sm')]: {
+      flexWrap: 'wrap',
+    },
   },
   tabs: {
     marginRight: theme.spacing(3),
@@ -59,18 +73,26 @@ const useStyles = makeStyles(theme => ({
   tab: {
     textTransform: 'capitalize',
   },
+  formControl: {
+    width: '100%',
+    marginBottom: theme.spacing(3),
+  },
   accordionContainer: {
     flex: 1,
   },
 }));
 
 const Faq = () => {
-  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
   const [tabValue, setTabValue] = useState(0);
+  const classes = useStyles();
 
   const handleChange = (event, newValue) => {
     setTabValue(newValue);
   };
+
+  const handleSelectChange = ev => setTabValue(Number(ev.target.value));
 
   return (
     <FullContainer maxWidth="xl">
@@ -80,29 +102,51 @@ const Faq = () => {
         component="h1"
       />
       <Box className={classes.container}>
-        <Paper variant="outlined" className={classes.tabs}>
-          <Tabs
-            orientation="vertical"
-            value={tabValue}
-            onChange={handleChange}
-            aria-label="Vertical tabs example"
-            indicatorColor="primary"
-          >
-            <Tab className={classes.tab} label="Getting Started" />
-            <Tab className={classes.tab} label="Broadcasting Live" />
-            <Tab className={classes.tab} label="AutoDJ Stream Automation" />
-            <Tab className={classes.tab} label="WebPlayer Page & Radio Player" />
-            <Tab className={classes.tab} label="Mobile Apps" />
-            <Tab className={classes.tab} label="Payments" />
-          </Tabs>
-        </Paper>
+        {matches ? (
+          <Paper variant="outlined" className={classes.tabs}>
+            <Tabs
+              orientation="vertical"
+              value={tabValue}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              indicatorColor="primary"
+            >
+              <Tab value={0} className={classes.tab} label="Getting Started" />
+              <Tab value={1} className={classes.tab} label="Broadcasting Live" />
+              <Tab value={2} className={classes.tab} label="AutoDJ Stream Automation" />
+              <Tab value={3} className={classes.tab} label="WebPlayer Page & Radio Player" />
+              <Tab value={4} className={classes.tab} label="Mobile Apps" />
+              <Tab value={5} className={classes.tab} label="Payments" />
+            </Tabs>
+          </Paper>
+        ) : (
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+            <Select
+              native
+              value={tabValue}
+              onChange={handleSelectChange}
+              label="Age"
+              inputProps={{
+                name: 'age',
+                id: 'outlined-age-native-simple',
+              }}
+            >
+              <option value={0}>Getting Started</option>
+              <option value={1}>Broadcasting Live</option>
+              <option value={2}>AutoDJ Stream Automation</option>
+              <option value={3}>WebPlayer Page & Radio Player</option>
+              <option value={4}>Mobile Apps</option>
+              <option value={5}>Payments</option>
+            </Select>
+          </FormControl>
+        )}
         <Box className={classes.accordionContainer}>
           <Accordion
             data={data}
             summaryProps={{
               variant: 'subtitle1',
               fontWeight: 500,
-              lineHeight: 1,
             }}
           />
         </Box>
