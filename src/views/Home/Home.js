efaf0e688947fe0f880bdcf7118edde0db8813a9
AWydra <hello@awydra.pl>
2020-07-22 13:24:40 +0200

    Translated home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,94 +1,87 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { Divider, Container } from '@material-ui/core';
 import Text from 'components/atoms/Text/Text';
-// import CTAButton from 'components/atoms/CTAButton/CTAButton';
 import FullContainer from 'components/atoms/FullContainer/FullContainer';
 import RowSection from 'components/organisms/RowSection/RowSection';
 import Accordion from 'components/organisms/Accordion/Accordion';
-// import RocketIcon from 'assets/svg/RocketIcon';
 import HeroSection from 'components/organisms/HeroSection/HeroSection';
 import history from 'utils/history';
 
-const heroData = {
-  heading: 'Add your Online Radio Station to Alexa',
-  pictures: {
-    mobile: 'https://img.fastcast4u.com/react/home/home-bg-mobile',
-    desktop: 'https://img.fastcast4u.com/react/home/home-bg',
-    alt: 'Alexa on a desk',
-  },
-  buttons: [
-    {
-      label: '\xa0GET NOW\xa0',
-      onClick: () => history.push('/alexa-skill'),
-      color: 'secondary',
-    },
-  ],
-};
-
-const sections = [
-  {
-    img: 'https://img.fastcast4u.com/react/home/device1',
-    heading: 'Start Streaming Online in 3\xa0minutes',
-    content: 'Complete Radio Server Package',
-    button: {
-      label: 'Start Now',
-      to: '/order',
-    },
-  },
-  {
-    img: 'https://img.fastcast4u.com/react/home/IPadIP',
-    heading: 'Mobile App',
-    content: 'Create Your Own Application for Android &\xa0iOS',
-    button: {
-      label: 'Start Now',
-      to: '/order',
-    },
-  },
-  {
-    img: 'https://img.fastcast4u.com/react/home/wpdev1',
-    heading: 'WebPlayer Page',
-    content: 'Customizable Radio Player for listeners',
-    button: {
-      label: 'Start Now',
-      to: '/order',
-    },
-  },
-  {
-    img: 'https://img.fastcast4u.com/react/home/alexa',
-    heading: 'Alexa Radio Skill',
-    content: 'Add your radio stream to Alexa',
-    button: {
-      label: 'Get Now',
-      to: '/alexa-skill',
-    },
-  },
-];
-
-const accordion = [
-  {
-    heading: 'How to start streaming online?',
-    content:
-      'With FastCast4u you can start streaming online in 3 minutes. Sign in, create a package and your Radio Server will be set up automatically, just upload music, broadcasts or shows or connect a live DJ from your computer.',
-  },
-  {
-    heading: 'Earn on streaming online',
-    content:
-      'SHOUTcast Radio Servers come with TargetSpot Stream Monetization program. You can also make money online by placing ad banners on your Web Player Page and activating Admob Ads on Mobile Apps. Lastly, your radio station can rely on Donates from satisfied listeners.',
-  },
-  {
-    heading: 'Web Player Page & Radio Player',
-    content:
-      'Web Player Page will automatically go online with your Radio Server. You don’t need web hosting, complicated website builders or domains - just customize the page and share with your listeners. The customize Radio Player is also available as an embed widget for websites.',
-  },
-  {
-    heading: 'Mobile Apps',
-    content:
-      'Mobile App for Android, iPhone & iPad features a Player for Online Radio and Live TV/Video streams. Your social media and website can be available in a built-in WebView. Using an Online App Creator you get a customized App with your own branding, artwork and theme.',
-  },
-];
-
 const Home = () => {
+  const content = useSelector(state => state.language.home);
+
+  const heroData = useMemo(
+    () => ({
+      heading: content.heroSection.heading,
+      pictures: {
+        mobile: 'https://img.fastcast4u.com/react/home/home-bg-mobile',
+        desktop: 'https://img.fastcast4u.com/react/home/home-bg',
+        alt: 'Alexa on a desk',
+      },
+      buttons: [
+        {
+          label: content.heroSection.buttons[0],
+          onClick: () => history.push('/alexa-skill'),
+          color: 'secondary',
+        },
+      ],
+    }),
+    [content],
+  );
+
+  const sections = useMemo(
+    () => [
+      {
+        img: 'https://img.fastcast4u.com/react/home/device1',
+        heading: content.rowSections[0].heading,
+        content: content.rowSections[0].content,
+        button: {
+          label: content.rowSections[0].button,
+          to: '/order',
+        },
+      },
+      {
+        img: 'https://img.fastcast4u.com/react/home/IPadIP',
+        heading: content.rowSections[1].heading,
+        content: content.rowSections[1].content,
+        button: {
+          label: content.rowSections[1].button,
+          to: '/order',
+        },
+      },
+      {
+        img: 'https://img.fastcast4u.com/react/home/wpdev1',
+        heading: content.rowSections[2].heading,
+        content: content.rowSections[2].content,
+        button: {
+          label: content.rowSections[2].button,
+          to: '/order',
+        },
+      },
+      {
+        img: 'https://img.fastcast4u.com/react/home/alexa',
+        heading: content.rowSections[3].heading,
+        content: content.rowSections[3].content,
+        button: {
+          label: content.rowSections[3].button,
+          to: '/alexa-skill',
+        },
+      },
+    ],
+    [content],
+  );
+
+  const accordion = useMemo(
+    () =>
+      content.faq.accordions.map(accordion => ({
+        heading: accordion.heading,
+        content: accordion.content,
+      })),
+    [content],
+  );
+
   return (
     <>
       {/* <FullContainer center maxWidth="xl">
@@ -118,7 +111,7 @@ const Home = () => {
       </FullContainer>
       <Container maxWidth="xl">
         <Text component="h2" variant="h3" mt={4} mb={6} align="center" fontWeight={500}>
-          Frequently Asked Questions
+          {content.faq.title}
         </Text>
         <Accordion data={accordion} />
       </Container>
