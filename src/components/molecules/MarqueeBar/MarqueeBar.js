5d5f96ae2ae3e45a62c9497668b3cb7880f3ee00
AWydra <hello@awydra.pl>
2020-09-09 18:48:10 +0200

    Removed marquee tag from Marquee component

--- src/components/molecules/MarqueeBar/MarqueeBar.js
+++ src/components/molecules/MarqueeBar/MarqueeBar.js
@@ -1,24 +1,30 @@
 /* eslint-disable jsx-a11y/no-distracting-elements */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import generalServices from 'services/general';
 import { Box, makeStyles } from '@material-ui/core';
-import Image from 'components/atoms/Image/Image';
+import Text from 'components/atoms/Text/Text';
 
 const useStyles = makeStyles({
-  marquee: {
-    display: 'block',
-  },
-  image: {
-    maxWidth: '80% !important',
+  text: {
+    textAlign: 'center',
+    color: 'white',
   },
 });
 
 const MarqueeBar = () => {
+  const [items, setItems] = useState('...');
   const classes = useStyles();
+
+  useEffect(() => {
+    generalServices.getItemsLeft('flashsaleapp').then(res => setItems(res.uses));
+  }, []);
+
   return (
-    <Box py={2} bgcolor="primary.main">
-      <marquee className={classes.marquee} behavior="alternate">
-        <Image className={classes.image} src="https://fastcast4u.com/img/flash/flash1w.png" />
-      </marquee>
+    <Box py={0.5} bgcolor="primary.main">
+      <Text className={classes.text} variant="h6">
+        Flash Sale: UNLIMITED Package + FREE Mobile App | LIMITED SUPPLY: {items} items left in
+        stock
+      </Text>
     </Box>
   );
 };
--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -10,8 +10,10 @@ import Logo from 'components/atoms/Logo/Logo';
 import NavButton from 'components/atoms/NavButton/NavButton';
 import SocialBar from 'components/molecules/SocialBar/SocialBar';
 import LanguageButton from 'components/molecules/LanguageButton/LanguageButton';
+import MarqueeBar from 'components/molecules/MarqueeBar/MarqueeBar';
 import { useCurrentLanguage } from 'utils/customHooks';
 import { isDev } from 'utils/nodeEnv';
+import { isNowBetween } from 'utils/date';
 
 import LightIcon from '@material-ui/icons/Brightness4';
 import DarkIcon from '@material-ui/icons/Brightness7';
@@ -47,19 +49,23 @@ const Header = () => {
   };
 
   return (
-    <StyledAppBar color="default" position="static" hidden={isHidden}>
-      <SocialBar />
-      <StyledToolbar>
-        <Logo mr="auto" to={`${lng}`} />
-        {isDev() && <LanguageButton />}
-        <Tooltip title={content.toggle}>
-          <IconButton color="primary" aria-label="toggle dark theme" onClick={handleClick}>
-            {theme === 'light' ? <LightIcon /> : <DarkIcon />}
-          </IconButton>
-        </Tooltip>
-        <NavButton to={`${lng}/login`}>{content.login}</NavButton>
-      </StyledToolbar>
-    </StyledAppBar>
+    <>
+      <StyledAppBar color="default" position="static" hidden={isHidden}>
+        <SocialBar />
+        <StyledToolbar>
+          <Logo mr="auto" to={`${lng}`} />
+          {isDev() && <LanguageButton />}
+          <Tooltip title={content.toggle}>
+            <IconButton color="primary" aria-label="toggle dark theme" onClick={handleClick}>
+              {theme === 'light' ? <LightIcon /> : <DarkIcon />}
+            </IconButton>
+          </Tooltip>
+          <NavButton to={`${lng}/login`}>{content.login}</NavButton>
+        </StyledToolbar>
+      </StyledAppBar>
+
+      {isNowBetween(Date.UTC(2020, 8, 10, 7), Date.UTC(2020, 8, 11, 7)) && <MarqueeBar />}
+    </>
   );
 };
 
